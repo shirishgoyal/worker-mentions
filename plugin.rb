c2d492f05afb2ff3898244b0c857ebc73bd492a0
Shirish Goyal <shirish.goyal@gmail.com>
2017-08-04 14:08:55 -0700

    settings for plugin

---
+++ plugin.rb
@@ -0,0 +1,10 @@
+# name: worker-mentions
+# about: Plugin to allow using @workers tag to mention all workers for a project
+# version: 0.0.1
+# authors: Daemo
+
+enabled_site_setting :worker_mentions_enabled
+enabled_site_setting :worker_mentions_group_name
+enabled_site_setting :worker_mentions_api
+
+
